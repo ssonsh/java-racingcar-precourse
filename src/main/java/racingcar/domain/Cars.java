2eec729ee87ee6d206ecf72b9f2654f2f59a355b
ssonsh <chosunci@gmail.com>
2022-04-20 22:12:22 +0900

    feat(Cars) : 자동차 도메인을 관리하는 1급 콜랙션 Cars 도메인을 생성한다. Car 도메인을 List Collection 형태로 관리하는 1급 콜랙션이다.
    - size(), add() 메소드를 기능으로 제공한다.

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,30 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Cars {
+    private List<Car> values;
+
+    private Cars(List<Car> values) {
+        this.values = values;
+    }
+
+    public static Cars from(List<Car> values) {
+        return new Cars(values);
+    }
+
+    public int size() {
+        if(isEmpty()){
+            return 0;
+        }
+        return this.values.size();
+    }
+
+    private boolean isEmpty() {
+        return this.values == null || this.values.isEmpty();
+    }
+
+    public void add(Car car) {
+        this.values.add(car);
+    }
+}
