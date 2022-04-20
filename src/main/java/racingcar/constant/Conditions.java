75aa629e7152803c22f3bcdf32b585ec5fdf66cb
ssonsh <chosunci@gmail.com>
2022-04-20 22:51:35 +0900

    feat(Engine) : 자동차의 전진/멈춤을 판단하는 처리기 개발 NumberGenerator 구현체를 통해 생성된 랜덤값을 이용해 전진/멈춤을 판단한다.
    - 4이상인 경우 전진
    - 3이하인 경우 멈춤

--- src/main/java/racingcar/constant/Conditions.java
+++ src/main/java/racingcar/constant/Conditions.java
@@ -4,4 +4,5 @@ public class Conditions {
     public static final int NAME_MAX_SIZE = 5;
     public static final int RACING_NUMBER_MIN = 0;
     public static final int RACING_NUMBER_MAX = 9;
+    public static final Integer MOVE_FORWARD_CONDITION = 4;
 }
