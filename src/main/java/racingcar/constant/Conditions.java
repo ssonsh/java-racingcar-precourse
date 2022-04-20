c4579edd39b1db58deac78936cbc637e33b83956
ssonsh <chosunci@gmail.com>
2022-04-20 22:33:50 +0900

    feat(Generator) : 자동차 전진/멈춤 처리를 위한 0~9사이 랜덤값 생성기 NumberGenerator 인터페이스를 정의하였고, 이를 구현한 RacingGameNumberGenerator 가 있다.
    - 제공되는 Randoms 유틸을 이용하여 0 ~ 9 사이의 랜덤값이 생성되도록 한다.

--- src/main/java/racingcar/constant/Conditions.java
+++ src/main/java/racingcar/constant/Conditions.java
@@ -2,4 +2,6 @@ package racingcar.constant;
 
 public class Conditions {
     public static final int NAME_MAX_SIZE = 5;
+    public static final int RACING_NUMBER_MIN = 0;
+    public static final int RACING_NUMBER_MAX = 9;
 }
